11b2c16b7d26f990190e0c95d6d28f304e6309c2
Danila Korzh <67706495+f3ll3n@users.noreply.github.com>
2024-05-31 19:44:08 +0300

    Time: 51 ms (61.61%), Space: 49.1 MB (67.36%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if(obj.length || Object.keys(obj).length){
+        return false
+    }
+    else{
+        return true
+    }
+};
